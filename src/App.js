6c8f002eee1394f24f4230af0007f803c64daaf1
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-10-27 14:15:35 +0200

    #2 - Setted routes component and implemented the 404 page.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      {/* Switch Component */}
+      <Switch>
+        {/* Dashboard */}
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        {/* Login */}
+        <Route path="/login">
+          <Login></Login >
+        </Route>
+        {/* Error page */}
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
