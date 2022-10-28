abc9f59454b8900cae07c035b7c74eff1a7ec4d4
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-10-28 17:19:31 +0200

    #8 - Settled and implemented data for the column components

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,40 +5,77 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 
 const Repos = () => {
   const { repos } = useContext(GithubContext)
+
   // Selecting all my languages
-  let languages = repos.reduce((total, item) => {
+  const languages = repos.reduce((total, item) => {
     // Selecting the "language" voice from items
-    const { language } = item
+    const { language, stargazers_count } = item
     // Statement if for the null result
     if (!language) return total;
     if (!total[language]) {
-      total[language] = 1
+      total[language] = { label: language, value: 1, stars: stargazers_count }
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
+      }
+      // Output
+      /* 
+      CSS: Object { label: "CSS", value: 38 }
+      HTML: Object { label: "HTML", value: 14 }
+      JavaScript: Object { label: "JavaScript", value: 45 }
+      */
     }
-    total[language] = 30
+
     return total
   }, {})
 
-  console.log(languages);
+  // Most used
+  const mostUsed = Object.values(languages).sort((a, b) => {
+    return b.value - a.value
+  }).slice(0, 5)
 
-  const chartData = [
-    {
-      label: "HTML",
-      value: "13"
-    },
-    {
-      label: "CSS",
-      value: "23"
-    },
-    {
-      label: "Javascript",
-      value: "80"
-    },
-  ];
+  // Most stars
+  const mostPopular = Object.values(languages).sort((a, b) => {
+    return b.stars - a.stars
+  }).map((item) => {
+    return { ...item, value: item.stars }
+  }).slice(0, 5)
+
+  // Most Stars / Forks
+
+  let { stars, forks } = repos.reduce((total, item) => {
+    // Destructuring of Item
+    const { stargazers_count, name, forks } = item;
+    // settled the total stars
+    total.stars[stargazers_count] = { label: name, value: stargazers_count }
+    // settled the total forks
+    total.forks[forks] = { label: name, value: forks }
+    return total
+  }, {
+    // Declared as an empty array
+    stars: {},
+    // Declared as an empty array
+    forks: {}
+  })
+  stars = Object.values(stars).slice(-5).reverse()
+  forks = Object.values(forks).slice(-5).reverse()
+
 
   return (
+    /* section */
     <section className="section">
+      {/* section-center */}
       <Wrapper className="section-center">
-        <Pie3D data={chartData} />
+        {/* Pie3D */}
+        <Pie3D data={mostUsed} />
+        {/* Column3D */}
+        <Column3D data={stars} />
+        {/* Doughnut2D */}
+        <Doughnut2D data={mostPopular} />
+        {/* Bar3D */}
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   );
@@ -66,7 +103,7 @@ const Wrapper = styled.div`
   svg {
     width: 100% !important;
     border-radius: var(--radius) !important;
-  }
+  } 
 `;
 
 export default Repos;
