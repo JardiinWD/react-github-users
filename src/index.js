5b00f057687cbdd5c57e0fe075f4e1620f2da94a
Alessandro Pecorilla <Pecorillaalessandro@gmail.com>
2022-10-29 13:49:02 +0200

    #11 - Implemented AuthProvider and Auth db from website.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+// Domain : dev-gx7ypnxahibu0t27.us.auth0.com
+// Client ID : NB4HF5yfNqX6LPQC8oq4LRmPwwtdpuGG
+
 ReactDOM.render(
   <React.StrictMode>
-    {/* My app component wrapped between the provider */}
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider domain="dev-gx7ypnxahibu0t27.us.auth0.com"
+      clientId="NB4HF5yfNqX6LPQC8oq4LRmPwwtdpuGG"
+      redirectUri={window.location.origin}>
+      {/* My app component wrapped between the provider */}
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
